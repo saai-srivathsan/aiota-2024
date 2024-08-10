2c1271a81b738939ee1c69d88ab0d2019cb44964
Dinesh-Kumar-E <dinesh-kumar-e@outlook.com>
2024-08-10 17:57:14 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build',  // Change the output directory to 'build'
+  },
   server: {
     host: '0.0.0.0',
     port: 5173,
